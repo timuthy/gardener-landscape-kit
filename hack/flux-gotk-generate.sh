9fbc4300ae634e4f2735efaed6e105c720ed0975
Tim Usner <tim.usner@sap.com>
2026-07-13 09:44:57 +0200

    Adjust Flux manifest generation

--- hack/flux-gotk-generate.sh
+++ hack/flux-gotk-generate.sh
@@ -40,3 +40,42 @@ sed -i 's|branch\:\s<branch>|{{ .repo_ref }}|g' $REPO_ROOT/pkg/components/flux/t
 ## Add comment for recurseSubmodules
 sed -i -e 's/recurseSubmodules: true/recurseSubmodules: true # required if Git submodules are used, e.g. to include the base repo in the landscape repo./g' \
   $REPO_ROOT/pkg/components/flux/templates/landscape/flux-system/gotk-sync.yaml
+
+## Extract controller images from gotk-components.yaml and update componentvector/components.yaml
+echo "> Updating Flux controller images in componentvector/components.yaml"
+
+GOTK=$REPO_ROOT/pkg/components/flux/templates/landscape/flux-system/gotk-components.yaml
+COMPONENTS=$REPO_ROOT/componentvector/components.yaml
+
+extract_image() {
+  grep -oP "(?<=image: )ghcr\.io/fluxcd/${1}:[^\s]+" "$GOTK" | head -1
+}
+
+SOURCE_IMAGE=$(extract_image "source-controller")
+KUSTOMIZE_IMAGE=$(extract_image "kustomize-controller")
+HELM_IMAGE=$(extract_image "helm-controller")
+NOTIFICATION_IMAGE=$(extract_image "notification-controller")
+
+GLK_INDEX=$(yq '.components | to_entries | .[] | select(.value.name == "github.com/gardener/gardener-landscape-kit") | .key' "$COMPONENTS")
+
+update_resource() {
+  local resource_key=$1
+  local image=$2
+  local repo="${image%:*}"
+  local tag="${image##*:}"
+  yq -i ".components[${GLK_INDEX}].resources.${resource_key}.ociImage.repository = \"${repo}\"" "$COMPONENTS"
+  yq -i ".components[${GLK_INDEX}].resources.${resource_key}.ociImage.tag = \"${tag}\"" "$COMPONENTS"
+}
+
+update_resource "sourceController" "$SOURCE_IMAGE"
+update_resource "kustomizeController" "$KUSTOMIZE_IMAGE"
+update_resource "helmController" "$HELM_IMAGE"
+update_resource "notificationController" "$NOTIFICATION_IMAGE"
+
+## Replace actual images in gotk-components.yaml with template placeholders
+echo "> Replacing images in gotk-components.yaml with template placeholders"
+
+sed -i "s|image: ${SOURCE_IMAGE}|image: {{ .resources.sourceController.ociImage.ref }}|g" "$GOTK"
+sed -i "s|image: ${KUSTOMIZE_IMAGE}|image: {{ .resources.kustomizeController.ociImage.ref }}|g" "$GOTK"
+sed -i "s|image: ${HELM_IMAGE}|image: {{ .resources.helmController.ociImage.ref }}|g" "$GOTK"
+sed -i "s|image: ${NOTIFICATION_IMAGE}|image: {{ .resources.notificationController.ociImage.ref }}|g" "$GOTK"
